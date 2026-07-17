7bdedc67aa252982298a0cc73ef0375a1f2100e0
kimmai291205-creator <kimmai291205@gmail.com>
2026-07-17 09:33:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-
+import streamlit as st
+st.title("🏦 Loan Approval App")
+name = st.text_input("Tên khách hàng")
+income = st.number_input("Thu nhập")
+loan = st.number_input("Khoản vay")
+if st.button("Đánh giá"):
+    if income > loan/20:
+        st.success("Đủ điều kiện vay")
+    else:
+        st.error("Không đủ điều kiện")
