b0a8718c28403418c9ac5b153b6c02783bdd36c4
kimmai291205-creator <kimmai291205@gmail.com>
2026-07-17 10:19:53 +0700

    Update 1_🏠_Trang_chu.py

--- pages/1_🏠_Trang_chu.py
+++ pages/1_🏠_Trang_chu.py
@@ -1,1 +1,12 @@
-
+import streamlit as st
+st.title("🏠 Trang chủ")
+st.write("""
+Chào mừng bạn đến với Hệ thống hỗ trợ thẩm định cho vay khách hàng cá nhân.
+Ứng dụng hỗ trợ:
+- Nhập hồ sơ khách hàng
+- Tính EMI
+- Tính DTI
+- Tính LTV
+- Chấm điểm CIC
+- Đưa ra quyết định cho vay
+""")
